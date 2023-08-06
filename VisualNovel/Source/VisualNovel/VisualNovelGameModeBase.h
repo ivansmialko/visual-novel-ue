006ef224b7e72edb01b99178a318c9710e666456
Ivan Smialko <ivan.smialko@ab-games.com>
2023-08-07 01:42:14 +0300

    Project set up

---
+++ VisualNovel/Source/VisualNovel/VisualNovelGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VisualNovelGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VISUALNOVEL_API AVisualNovelGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
